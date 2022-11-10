00ed33a5d7b9d9d95e82c9a810e34ebffae5926d
anhquan74-dev <anhquan74.dev@gmail.com>
2022-11-10 17:10:38 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import GlobalStyles from "./components/GlobalStyles";
+
+function App() {
+  return (
+    <GlobalStyles>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    </GlobalStyles>
+  );
+}
+export default App;
